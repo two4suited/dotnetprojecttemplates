6b0bd11684a99efc9b7d77606ef5e4679471892d
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-22 10:44:22 -0800

    Working template for creating a project

--- installtemplates.sh
+++ installtemplates.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 set -u
 
-function RemoveExtraFiles(){
-    rm -r .idea
-    rm -r bin
-    rm -r obj
-}
 
 function InstallTemplate(){
     dotnet new -i ./
 }
 
 function InstallLambdaWithDI(){
-    cd LambdawithDI/src/LambdawithDI.Functions
-    RemoveExtraFiles
+    cd LambdawithDI
     InstallTemplate
 }
 
